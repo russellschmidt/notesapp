2b670332f312937236799e3d0bf630f1b83db10a
Russell Schmidt <russell.schmidt@me.com>
2021-04-21 23:39:34 -0700

    add and delete notes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {useEffect, useReducer} from 'react'
 import { API } from 'aws-amplify'
-import { List } from 'antd'
+// import { List } from 'antd'
 import 'antd/dist/antd.css'
 import { listNotes } from './graphql/queries'
+import { 
+  createNote as CreateNote, 
+  deleteNote as DeleteNote 
+} from './graphql/mutations'
+import { v4 as uuid } from 'uuid'
+import { List, Input, Button } from 'antd'
 
 import './App.css';
+
+const CLIENT_ID = uuid()
 
 const initialState = {
   notes: [],
@@ -19,6 +27,12 @@ function reducer(state, action) {
       return {...state, notes: action.notes, loading: false}
     case 'ERROR':
       return { ...state, loading: false, error: true}
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]}
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } }
     default:
       return state
   }
@@ -26,6 +40,43 @@ function reducer(state, action) {
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  async function createNote() {
+    const { form } = state
+    if (!form.name || !form.description) {
+      return alert('Please enter a name and description')
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('Success!! Created note')
+    } catch (err) {
+      console.log("error: ", err)
+    }
+  }
+
+  async function deleteNote({ id }) {
+    const index = state.notes.findIndex(n => n.id === id)
+    const notes = [
+      ...state.notes.slice(0, index),
+      ...state.notes.slice(index + 1)
+    ]
+    dispatch({ type: 'SET_NOTES', notes })
+    try {
+      await API.graphql({
+        query: DeleteNote,
+        variables: { input: { id } }
+      })
+      console.log('successfully deleted note!')
+    } catch (err) {
+      console.log({ err })
+    }
+  }
 
   async function fetchNotes() {
     try {
@@ -46,10 +97,18 @@ export default function App() {
     fetchNotes()
   }, [])
 
+  function onChange(e) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
+  }
 
   function renderItem(item) {
     return (
-      <List.Item style={styles.item}>
+      <List.Item 
+        style={styles.item}
+        actions={[
+          <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>
+        ]}
+      >
         <List.Item.Meta 
           title={item.name}
           description={item.description}
@@ -67,6 +126,30 @@ export default function App() {
 
   return (
     <div style={styles.container}>
+
+      <form>
+        <Input 
+          onChange={onChange}
+          value={state.form.name}
+          placeholder="Note Name"
+          name='name'
+          style={styles.input}
+        />
+        <Input 
+          onChange={onChange}
+          value={state.form.description}
+          placeholder="Note description"
+          name="description"
+          style={styles.input}
+        />
+        <Button
+          onClick={createNote}
+          type="primary"
+        >
+          Create Note
+        </Button>
+      </form>
+
       <List 
         loading={state.loading}
         dataSource={state.notes}
